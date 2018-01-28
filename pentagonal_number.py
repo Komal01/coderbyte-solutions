17bd86dc724c3fe8e2f8353a5cfef555f06efaac
Komal Maheshwari <komalmaheshwari36@gmail.com>
2018-01-28 23:48:40 +0530

    Update pentagonal_number.py

--- pentagonal_number.py
+++ pentagonal_number.py
@@ -1,3 +1,7 @@
+#Using the Python language, have the function PentagonalNumber(num) read num which will be a positive integer and 
+#determine how many dots exist in a pentagonal shape around a center dot on the Nth iteration. 
+
+
 def PentagonalNumber(num): 
     a=1+5*(num-1)*num/2
     return a
